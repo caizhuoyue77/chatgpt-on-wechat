2ad42ed75aef4da5c504833a4da7346d4719ef9d
caizhuoyue <caizhuoyue77@gmail.com>
2024-08-16 18:10:35 +0800

    Change bot_factory.py to always use Minimax

--- bot/bot_factory.py
+++ bot/bot_factory.py
@@ -10,6 +10,10 @@ def create_bot(bot_type):
     :param bot_type: bot type code
     :return: bot instance
     """
+    # Use Minimax no matte what
+    from bot.minimax.minimax_bot import MinimaxBot
+    return MinimaxBot()
+
     if bot_type == const.BAIDU:
         # 替换Baidu Unit为Baidu文心千帆对话接口
         # from bot.baidu.baidu_unit_bot import BaiduUnitBot
